e846031c8a86b2bac3b5bef29c7c6f182e37cee6
govind somalal <govindsomalal7@gmail.com>
2018-12-10 11:57:25 +0530

    minor fix

--- models/sk_mnist/runtime/SkMnist.py
+++ models/sk_mnist/runtime/SkMnist.py
@@ -3,7 +3,7 @@ from sklearn.externals import joblib
 class SkMnist(object):
     def __init__(self):
         self.class_names = ["class:{}".format(str(i)) for i in range(10)]
-        self.clf = joblib.load('/data/sk.pkl') 
+        self.clf = joblib.load('/data/mymodel.pkl') 
 
     def predict(self,X,feature_names):
         predictions = self.clf.predict_proba(X)
