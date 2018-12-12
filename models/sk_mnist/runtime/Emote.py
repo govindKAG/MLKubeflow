3cdc9a75f72cb3d7737d9cffa069bdf9385b6230
govind somalal <govindsomalal7@gmail.com>
2018-12-12 17:29:28 +0530

    bugfix

--- models/sk_mnist/runtime/Emote.py
+++ models/sk_mnist/runtime/Emote.py
@@ -4,12 +4,12 @@ import numpy as np
 
 class Emote(object):
     def __init__(self):
-        #self.class_names = ["class:{}".format(str(i)) for i in range(10)]
+        self.class_names = ["class:{}".format(str(i)) for i in range(2)]
         self.clf = joblib.load('/data/mymodel.pkl') 
 
     def predict(self,X,feature_names):
         vectorizer = joblib.load('/data/vect.pkl') 
         feat = vectorizer.transform(X[0])
-        return np.asarray([self.clf.predict(feat)[0]])
+        return np.asarray([[self.clf.predict(feat)[0]]])
 
     
