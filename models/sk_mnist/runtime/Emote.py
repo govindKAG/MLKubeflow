ba62f55c96e36d7dcf2c18146235ca8ae8e97cfd
govind somalal <govindsomalal7@gmail.com>
2018-12-12 14:21:05 +0530

    changed to identity function for testing

--- models/sk_mnist/runtime/Emote.py
+++ models/sk_mnist/runtime/Emote.py
@@ -3,11 +3,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 class SkMnist(object):
     def __init__(self):
-        self.class_names = ["class:{}".format(str(i)) for i in range(10)]
+        #self.class_names = ["class:{}".format(str(i)) for i in range(10)]
         self.clf = joblib.load('/data/mymodel.pkl') 
 
     def predict(self,X,feature_names):
-        predictions = self.clf.predict_proba(X)
-        return predictions
+        return X
 
     
