450f3d2cc6380edfe7dfb4ce204d2721fd04abac
govind somalal <govindsomalal7@gmail.com>
2019-02-11 17:11:15 +0530

    completed initial poc of yaml generation with dynamic injection of runtime commands

--- models/sk_mnist/runtime/demo-scripts/yaml-gen-ruamel.py
+++ models/sk_mnist/runtime/demo-scripts/yaml-gen-ruamel.py
@@ -1,13 +1,18 @@
 from ruamel.yaml import YAML
+from splitter import get_args
 
 yaml = YAML()
 yaml.preserve_quotes = True
 
-
+arglist = get_args()
+print(arglist)
+transformed = ['"--' +str(i[0]) + '=' + str(i[1]) + '"'  for i in arglist]
+print(','.join(transformed))
+tranformed_str = '['+','.join(transformed) + ']'
 with open('new-training.yaml', 'r') as f:
     ogfile = yaml.load(f)
 
-ogfile['spec']['templates'][2]['resource']['manifest'] = ogfile['spec']['templates'][2]['resource']['manifest'].format(command='python -u', args='["--message = new message", "--count=10"]')
+ogfile['spec']['templates'][2]['resource']['manifest'] = ogfile['spec']['templates'][2]['resource']['manifest'].format(command='python -u', args=tranformed_str)
 
 
 with open("my_file.yaml", "w") as f:
