23e25ab22d8f2a5ef1b859b5af54a2837a6f2316
govind somalal <govindsomalal7@gmail.com>
2019-02-13 14:15:26 +0530

    split command and args field handling

--- models/sk_mnist/runtime/demo-scripts/splitter.py
+++ models/sk_mnist/runtime/demo-scripts/splitter.py
@@ -1,7 +1,19 @@
-
+import shlex
 def get_args():
     l = []
     with open('sample-args.txt','r') as f:
+        command = f.readline().strip()
+        l.append(shlex.split(command))
         for i in f:
             l.append(i.strip().split(':'))
     return l
+
+#res = get_args()
+
+#print(res)
+#command = res.pop(0)
+#print(command)
+#print(res)
+#command_str = [ '"' + i +'"' for i in command]
+#command_str = '[' + ','.join(command_str) + ']'
+#print(command_str)
--- models/sk_mnist/runtime/demo-scripts/yaml-gen-ruamel.py
+++ models/sk_mnist/runtime/demo-scripts/yaml-gen-ruamel.py
@@ -5,6 +5,9 @@ yaml = YAML()
 yaml.preserve_quotes = True
 
 arglist = get_args()
+command = arglist.pop(0)
+command_str = [ '"' + i +'"' for i in command]
+command_str = '[' + ','.join(command_str) + ']'
 print(arglist)
 transformed = ['"--' +str(i[0]) + '=' + str(i[1]) + '"'  for i in arglist]
 print(','.join(transformed))
@@ -12,7 +15,7 @@ tranformed_str = '['+','.join(transformed) + ']'
 with open('new-training.yaml', 'r') as f:
     ogfile = yaml.load(f)
 
-ogfile['spec']['templates'][2]['resource']['manifest'] = ogfile['spec']['templates'][2]['resource']['manifest'].format(command='["python -u create_model.py"]', args=tranformed_str)
+ogfile['spec']['templates'][2]['resource']['manifest'] = ogfile['spec']['templates'][2]['resource']['manifest'].format(command=command_str, args=tranformed_str)
 
 
 with open("my_file.yaml", "w") as f:
