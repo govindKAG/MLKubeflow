eb5263ef1292f32d08d9d2e21d253d969f28b177
govind somalal <govindsomalal7@gmail.com>
2019-02-18 10:57:39 +0530

    added run and train files for make

---
+++ models/sk_mnist/runtime/demo-scripts/run_build.py
@@ -0,0 +1,3 @@
+from build import build
+
+build('new-training.yaml', 'my_file.yaml', docker_user = 'govindkag', github_user = 'govindKAG', train_name = 'emote-trainb33', version='4.4')
---
+++ models/sk_mnist/runtime/demo-scripts/run_train.py
@@ -0,0 +1,2 @@
+from build import train
+train('new-training.yaml', 'my_file.yaml', docker_user = 'govindkag', github_user = 'govindKAG', train_name = 'emote-trainb33', version='4.4')
